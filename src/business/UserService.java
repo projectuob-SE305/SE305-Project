d99b898996b4a3cb189c889d65aec059c7419de5
Hassan Ali <hassanaliatyea@gmail.com>
2026-03-14 20:18:56 +0300

    layers

---
+++ src/business/UserService.java
@@ -0,0 +1,42 @@
+package business;
+import data.User;
+
+import data.UserRepository;
+
+/**
+ * Business Layer responsible for user account operations.
+ */
+public class UserService {
+
+    private final UserRepository repository;
+
+    public UserService() {
+        repository = new UserRepository();
+    }
+
+    // Register new user
+    public void register(String name, String email, String password) {
+
+        User existingUser = repository.findUserByEmail(email);
+
+        if (existingUser != null) {
+            System.out.println("User already exists.");
+            return;
+        }
+
+        User newUser = new User(name, email, password);
+        repository.addUser(newUser);
+    }
+
+    // Login verification
+    public boolean login(String email, String password) {
+
+        User user = repository.findUserByEmail(email);
+
+        if (user == null) {
+            return false;
+        }
+
+        return user.getPassword().equals(password);
+    }
+}
