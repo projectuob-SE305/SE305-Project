b66afbe35d9d6def58c9bbe71364f5fbe08dda4f
Hassan Ali <hassanaliatyea@gmail.com>
2026-04-23 22:01:27 +0300

    actions

--- src/test/java/UserRepositoryTest.java
+++ src/test/java/UserRepositoryTest.java
@@ -1,5 +1,5 @@
-package data;
-
+import data.User;
+import data.UserRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
